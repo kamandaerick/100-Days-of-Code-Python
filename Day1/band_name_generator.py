4b2406c6e211db78212a42fe5c4a9ddfd252da17
Kamanda <erickckamanda>
2023-12-23 13:27:53 +0300

    Generate a band name using user's city and pet names

---
+++ Day1/band_name_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This script generates a band name from a user's city and pet names"""
+
+
+print('Hello, welcome to my band name generator program\n')
+
+city_name = input('In what city did you grow up?\n')
+
+pet_name = input('What is the name of your pet?\n')
+
+print(f'Your band name is {city_name} {pet_name}')
