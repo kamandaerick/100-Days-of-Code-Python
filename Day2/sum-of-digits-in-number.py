b975dc1367be85f4a52187390d5e6c44af91948d
Kamanda <erickckamanda>
2024-01-28 12:19:32 +0300

    Add a statement to ask the uer for the inpput

--- Day2/sum-of-digits-in-number.py
+++ Day2/sum-of-digits-in-number.py
@@ -1,5 +1,5 @@
 """Ask the user to enter a two digit number"""
-two_digit_number = input()
+two_digit_number = input("Enter a two-digit number: ")
 """Inputs are usually strings. Convert them to integers and perfom the summation"""
 result = int(two_digit_number[0]) + int(two_digit_number[1])
 """Print out the output"""
