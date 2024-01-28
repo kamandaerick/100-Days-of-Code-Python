3f6c3cf134e4be79222e65133aebfaac962a3423
Kamanda <erickckamanda>
2024-01-28 12:48:00 +0300

    BMI calculator in metric units

---
+++ Day2/bmi-claculator.py
@@ -0,0 +1,13 @@
+"""This script accets the weight (kgs) and height (m) and calculates the BMI"""
+
+"""Input the height"""
+height = input("Height: ")
+
+"""Input the weight"""
+weight = input("Weight: ")
+
+"""Calculate the BMI and print result as an integer"""
+bmi = float(weight) / (float(height) ** 2)
+
+"""Print the bmi"""
+print(int(bmi))
