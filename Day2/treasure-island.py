a1743fa2edf06a3d6b4e80689e900a656881335b
Kamanda <erickckamanda>
2024-02-01 16:49:12 +0300

    Add reasons for ending the game

--- Day2/treasure-island.py
+++ Day2/treasure-island.py
@@ -3,17 +3,17 @@ print("Welcome to Treasure Island. Your mission is to find the treasure.")
 
 direction = input("Where would you like to go first? Left or Right? ")
 if direction == "Right":
-  print("Game Over")
+  print("You fell into a hole. \nGame Over")
   exit()
 else:
   swim_or_wait = input("Would you like to Swim o Wait? ")
   if swim_or_wait == "Swim":
-    print("Game Over")
+    print("The water has Sharks and Crocodiles.\nGame Over")
     exit()
   else:
     chosen_door = input("Which door would you like to enter? Red, Blue, or Yellow? ")
     if chosen_door == "Blue" or chosen_door == "Red":
-      print("Game over")
+      print("We don't have the key to that door.\nGame over")
       exit()
     else:
       print("You win.")
